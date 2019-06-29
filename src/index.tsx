58b2669c9d35a1d6e30c0fd78a3577d41405dfe6
Brian Smiley <briantsmiley42@gmail.com>
2019-06-29 16:36:07 -0400

    installs react types and convert Hello World from JS to React

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
+// function component() {
+//     const element = document.createElement('div');
+
+//     element.innerHTML = 'Hello webpack'
+
+//     return element;
+//   }
+
+//   document.body.appendChild(component());
+
+import React from "react";
+import ReactDom from "react-dom";
+
 function component() {
-    const element = document.createElement('div');
-    
-    element.innerHTML = 'Hello webpack'
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  return (
+    <div>
+      <br />Hello react
+    </div>
+  );
+}
+
+ReactDom.render(component(), document.body);
